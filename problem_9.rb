63b0c3bf53bdf6a65a74ffbd920e4131043d5cf4
Jordan Hudgens <jordan@wow.com>
2014-12-22 17:10:22 -0600

    added comments

--- problem_9.rb
+++ problem_9.rb
@@ -4,7 +4,7 @@
     @sum = n
   end
 
-  # Method uses nested loops to iterate through and add to collection
+  # Method uses nested loops to iterate through and add to result collection
   def pythagorean_triplet(num)
     result = []
     (2..num).each do |m|
@@ -15,10 +15,14 @@
     result
   end
 
-  # Calls method recursively and only returns if a + b + c = n
+  # Iterates through the returned result of the pythagorean_triplet method
   def pythagorean_triplet_printer
     pythagorean_triplet(@sum/2).each do |triplet|
+
+      # assigns the three collection values to an array
       a, b, c = triplet[0], triplet[1], triplet[2]
+      
+      # Checks with each iteration if the iteration value is equal to the sum of the three values
       if a + b + c == @sum
         puts a * b * c
         break
