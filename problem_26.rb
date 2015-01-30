3a3b4b3fb8a27c70968f57c8a05a347439a84e6a
Jordan Hudgens <jordan@wow.com>
2015-01-29 20:53:52 -0600

    added problem 26

---
+++ problem_26.rb
@@ -0,0 +1,30 @@
+# Solution from: http://lousycoder.blogspot.com/2010/01/project-euler-problem-26.html
+
+class Array
+  def tail
+    self.drop(1)
+  end
+end
+
+def order(a, m)
+  b = 1
+  (1...m).each do |i|
+    b = (a*b)%m
+    return i if b == 1
+  end
+end
+
+def max_period(best, numbers)
+  return best if numbers.nil?
+  n = numbers.first
+  ord = order(10, n)
+  return [n, ord] if ord == n - 1
+  return max_period([n, ord], numbers.tail) if ord > best.last
+  return max_period(best, numbers.tail)
+end
+
+numbers = 999.step(2, -1).select { |n| n % 2 != 0 && n % 5 != 0 }
+result = max_period([0, 0], numbers)
+puts result.first
+
+# Solution => 983
